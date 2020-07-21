a6b05cc6c23959e2c205203b5cff7b978a92bd7f
khokhriya <khokhriya.drpk@gmail.com>
2020-07-21 20:22:36 +0530

    areas

---
+++ areas.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import math
+
+def triangle(base,height):
+    return base*height/2
+
+def rectangle(bas3e,height):
+    return base*height
+
+def circle(radius):
+    return math.pi*(radius**2)
+
